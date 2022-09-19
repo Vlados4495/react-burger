65cc18ac58e70d4ea08f7be0cacb9559d2bf4554
Vladislav Loginov <loginov.vlados@yandex.ru>
2022-09-19 22:16:26 +0300

    create new component

---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import tabsStyles from './tabs.module.css'
+
+function Tabs() {
+    const [current, setCurrent] = useState("bun");
+    return (
+        <div className={tabsStyles.container}>
+            <Tab value="bun" active={current === "bun"} onClick={setCurrent}>
+                Булки
+            </Tab>
+            <Tab value="sause" active={current === "sause"} onClick={setCurrent}>
+                Соусы
+            </Tab>
+            <Tab value="main" active={current === "main"} onClick={setCurrent}>
+                Начинки
+            </Tab>
+        </div>
+    );
+}
+
+export default Tabs;

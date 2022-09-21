5349d8422890bcc37d6c1aac285ea6f75236dacc
Vladislav Loginov <loginov.vlados@yandex.ru>
2022-09-21 18:13:37 +0300

    add bugerconstructor component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import AppStyles from './App.module.css';
 import AppHeader from '../app-header/app-header';
 import { burgerIngredientsData } from '../../utils/data';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients'
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstrucor from '../burger-constructor/burger-constructor';
 
 function App() {
     return (
         <div className={AppStyles.wrapper}>
             <AppHeader />
             <BurgerIngredients ingredients={burgerIngredientsData} />
+            <BurgerConstrucor ingredients={burgerIngredientsData} />
         </div>
     )
 }
